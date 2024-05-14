ace1d5c1b572625f8a84554b0bcc655311258268
Kay Grewe <Kay.Grewe@protonmail.com>
2024-05-14 20:06:23 +0200

    refactor(core): remove unnecessary constants

--- src/cpu/core.rs
+++ src/cpu/core.rs
@@ -7,18 +7,6 @@ use super::register::Register;
 use crate::Memory;
 use crate::RegisterFile;
 
-const DECODER_OPCODE_MASK: u32 = 0x3FF;
-const DECODER_DESTINATION_REGISTER_START: u32 = 10;
-const DECODER_DESTINATION_REGISTER_MASK: u32 = 0x3F;
-const DECODER_SOURCE_REGISTER_START: u32 = 16;
-const DECODER_SOURCE_REGISTER_MASK: u32 = 0x3F;
-const DECODER_OFFSET_START: u32 = 22;
-const DECODER_OFFSET_MASK: u32 = 0x3F;
-const DECODER_SIZE_START: u32 = 28;
-const DECODER_SIZE_MASK: u32 = 0x03;
-const DECODER_INCREMENT_START: u32 = 30;
-const DECODER_INCREMENT_MASK: u32 = 0x03;
-
 pub struct Cpu {
     registers: RegisterFile,
     status_register: u32,
--- src/cpu/opcode.rs
+++ src/cpu/opcode.rs
@@ -1,19 +1,5 @@
 use super::{addressing_modes::AddressingMode, opcode_size::OpcodeSize};
 
-const DECODER_OPCODE_MASK: u32 = 0x3FF;
-
-const DECODER_DESTINATION_REGISTER_START: u32 = 10;
-const DECODER_DESTINATION_REGISTER_MASK: u32 = 0x3F;
-
-const DECODER_SOURCE_REGISTER_START: u32 = 16;
-const DECODER_SOURCE_REGISTER_MASK: u32 = 0x3F;
-
-const DECODER_OFFSET_START: u32 = 22;
-const DECODER_OFFSET_MASK: u32 = 0x3F;
-
-const DECODER_SIZE_START: u32 = 28;
-const DECODER_SIZE_MASK: u32 = 0x03;
-
 #[derive(Debug, PartialEq, Eq)]
 pub enum Opcode {
     Move(MoveOpcode),
