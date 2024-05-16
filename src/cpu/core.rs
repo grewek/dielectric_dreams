d2d9c6e6b74fe03e6255c2fb57fe552e85f086c9
Kay Grewe <Kay.Grewe@protonmail.com>
2024-05-16 19:19:12 +0200

    chore(core): remove unnecessary comments

--- src/cpu/core.rs
+++ src/cpu/core.rs
@@ -14,11 +14,6 @@ pub struct Cpu {
     memory: Memory,
 }
 
-//TODO(Kay): If i really want to put this on a FPGA later i should make sure this fever dream is working on real electronic circuits...but i don't have a degree in electronics... :D
-//TODO(Kay): We leave the flags out for now as it is still to be determined how that should work :)
-//TODO(Kay): We will take a bit of inspiration from the good old MOTOROLA 68K :)
-//TODO(Kay): It might be that i misjudged the amount of opcodes i need!
-
 impl Cpu {
     pub fn new() -> Self {
         Self {
@@ -29,45 +24,9 @@ impl Cpu {
         }
     }
 
-    /*pub fn addressing_mode(&self, increment_mode: u32, register: u32) -> AddressingMode {
-        let addr_mode_bits = register >> 4;
-
-        let increment = increment_mode & 0x01 == 0x01;
-        let decrement = increment_mode & 0x02 == 0x02;
-
-        match addr_mode_bits {
-            0x00..=0x01 => AddressingMode::Atomic(Register::decode(register)),
-            0x03 if increment => AddressingMode::MemoryInc(Register::decode(register)),
-            0x03 if decrement => AddressingMode::MemoryDec(Register::decode(register)),
-            //TODO(Kay): We should rethink the ISA things are getting fiddly already...
-            0x03 => AddressingMode::Memory(Register::decode(register)),
-            _ => unreachable!(),
-        }
-    }*/
-
     pub fn decoder(&self, to_decode: u32) -> Opcode {
         let raw_opcode = BitPattern::new(to_decode);
         raw_opcode.into()
-        /*let opcode = to_decode & DECODER_OPCODE_MASK;
-        let dest =
-            (to_decode >> DECODER_DESTINATION_REGISTER_START) & DECODER_DESTINATION_REGISTER_MASK;
-        let src = (to_decode >> DECODER_SOURCE_REGISTER_START) & DECODER_SOURCE_REGISTER_MASK;
-
-        //let src_immediate_value = src & 0x20;
-
-        let offset = (to_decode >> DECODER_OFFSET_START) & DECODER_OFFSET_MASK;
-        let size = (to_decode >> DECODER_SIZE_START) & DECODER_SIZE_MASK;
-        let increment = (to_decode >> DECODER_INCREMENT_START) & DECODER_INCREMENT_MASK;
-
-        match opcode {
-            0x01 => Opcode::Move(MoveOpcode {
-                destination: self.addressing_mode(increment, dest), //AddressingMode::Atomic(Register::new(dest)),
-                source: self.addressing_mode(increment, src), //AddressingMode::Atomic(Register::new(src)),
-                offset,
-                size: OpcodeSize::new(size),
-            }),
-            _ => todo!(),
-        }*/
     }
 }
 
