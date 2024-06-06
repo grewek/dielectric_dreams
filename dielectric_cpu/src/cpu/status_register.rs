a501c912ad27b1fc144b8fec69972e77c2649d4f
Kay Grewe <Kay.Grewe@protonmail.com>
2024-06-06 18:21:59 +0200

    feat(status_register): add beginnings of the status register

---
+++ dielectric_cpu/src/cpu/status_register.rs
@@ -0,0 +1,36 @@
+enum Flags {
+    Negative = 0x8000,  //The value is negative
+    Overflow = 0x4000,  //A Over/Underflow occured while doing the operation
+    Carry = 0x2000,     //The value did not fit and it carried a one into the status register
+    Parity = 0x1000, //If the amount of Bits set in the last operation was even this bit is set if it is odd it will be off
+    Zero = 0x0800,   //The value was zero...
+    Interrupt = 0x0400, //Interrupt flag used to indicate that something important happended
+    Trap = 0x0200,   //Trapflag used for debugging
+                     //...
+}
+
+struct StatusRegister {
+    flags: u16,
+}
+
+impl StatusRegister {
+    fn new() -> Self {
+        Self { flags: 0x00 }
+    }
+
+    fn raise(&mut self, flag: Flags) {
+        let target_flag = flag as usize;
+        self.flags |= 1 << target_flag;
+    }
+
+    fn clear(&mut self, flag: Flags) {
+        let target_flag = flag as usize;
+        self.flags &= 1 << target_flag;
+    }
+
+    fn status_bits(&self) -> u16 {
+        self.flags
+    }
+}
+
+//TODO(Kay): Testing!
