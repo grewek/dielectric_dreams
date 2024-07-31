21f280e04cbd55c751a7b5bb50c1e22cabce8092
Kay Grewe <Kay.Grewe@protonmail.com>
2024-07-31 23:14:00 +0200

    feat(assembler): test the immediate 32bit generator

--- assembler/src/main.rs
+++ assembler/src/main.rs
@@ -1,7 +1,7 @@
 use assembler::Assembler;
 
 fn main() {
-    let source = "start:\nnop move.dw D0, D7";
+    let source = "start:\nnop move.dw D0, D7\nmove.dw D2, $AABBCCDD";
     let mut assembler = Assembler::new();
     assembler.assemble(source);
 }
