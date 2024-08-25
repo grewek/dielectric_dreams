394f8049e82d8387b857721bfe692059ea2df872
Kay Grewe <Kay.Grewe@protonmail.com>
2024-08-25 11:10:34 +0200

    feat(krisp): add a lexer for better scanning of tokens

---
+++ krisp/src/lexer.c
@@ -0,0 +1,109 @@
+#include <stdio.h>
+#include <assert.h>
+#include <stdbool.h>
+
+#include "lexer.h"
+
+Lexer NewLexer(const char *source)
+{
+    Lexer result = {.source = source};
+    return result;
+}
+
+bool IsDigit(u8 digit)
+{
+    if ((digit >= '0' && digit <= '9'))
+    {
+        return true;
+    }
+
+    return false;
+}
+
+bool IsAlpha(u8 letter)
+{
+    if ((letter >= 'A' && letter <= 'Z') || (letter >= 'a' && letter <= 'z'))
+    {
+        return true;
+    }
+
+    return false;
+}
+
+Token DigestIdentifier(Lexer *lexer)
+{
+    u8 *start = lexer->source;
+    u8 *end = lexer->source;
+
+    while (*lexer->source && IsAlpha(*lexer->source))
+    {
+        end += 1;
+        *lexer->source++;
+    }
+
+    // TODO: For now we can only match dump tokens all the other identifiers are lost in time..
+    Token result = {.tt = TOKEN_TYPE_DUMP, .tokenStart = start, .tokenEnd = end, .length = end - start};
+
+    return result;
+}
+
+Token DigestValue(Lexer *lexer)
+{
+    u8 *start = lexer->source;
+    u8 *end = lexer->source;
+    while (*lexer->source && IsDigit(*lexer->source))
+    {
+        end += 1;
+        *lexer->source++;
+    }
+
+    assert(start != end);
+
+    Token result = {.tt = TOKEN_TYPE_VALUE,
+                    .tokenStart = start,
+                    .tokenEnd = end,
+                    .length = end - start};
+
+    return result;
+}
+
+Token NextToken(Lexer *lexer)
+{
+    fprintf(stdout, "current source: %s\n", lexer->source);
+
+    while (*lexer->source)
+    {
+        if (*lexer->source == '(')
+        {
+            Token result = {.tt = TOKEN_TYPE_OPEN_PAREN, .tokenStart = lexer->source, .tokenEnd = lexer->source, .length = 1};
+            *lexer->source++;
+            return result;
+        }
+        else if (*lexer->source == ')')
+        {
+            Token result = {.tt = TOKEN_TYPE_CLOSE_PAREN, .tokenStart = lexer->source, .tokenEnd = lexer->source, .length = 1};
+            *lexer->source++;
+            return result;
+        }
+        else if (*lexer->source == '+')
+        {
+            Token result = {.tt = TOKEN_TYPE_PLUS, .tokenStart = lexer->source, .tokenEnd = lexer->source, .length = 1};
+            *lexer->source++;
+            return result;
+        }
+        else if (*lexer->source == '_' || IsAlpha(*lexer->source))
+        {
+            return DigestIdentifier(lexer);
+        }
+        else if (IsDigit(*lexer->source))
+        {
+            return DigestValue(lexer);
+        }
+
+        *lexer->source++;
+    }
+
+    assert(*lexer->source == NULL);
+    Token result = {.tt = TOKEN_TYPE_EOF, .tokenStart = lexer->source, .tokenEnd = lexer->source, .length = 1};
+    return result;
+}
---
+++ krisp/src/lexer.h
@@ -0,0 +1,31 @@
+#pragma once
+#include "types.h"
+
+extern u8 tempBuffer[4096];
+
+typedef enum
+{
+    TOKEN_TYPE_OPEN_PAREN,
+    TOKEN_TYPE_CLOSE_PAREN,
+    TOKEN_TYPE_VALUE,
+    TOKEN_TYPE_PLUS,
+    TOKEN_TYPE_DUMP,
+    TOKEN_TYPE_EOF,
+
+} TokenType;
+
+typedef struct
+{
+    const u8 *source;
+} Lexer;
+
+typedef struct
+{
+    TokenType tt;
+    const u8 *tokenStart;
+    const u8 *tokenEnd;
+    usize length;
+} Token;
+
+Lexer NewLexer(const char *source);
+Token NextToken(Lexer *lexer);
