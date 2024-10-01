6a5fa674be9a12b3a2df71ebde35d71ab1b1c8c6
Kay Grewe <Kay.Grewe@protonmail.com>
2024-10-01 21:08:58 +0200

    feat(assembler_protomain): assembly push manual test

--- assembler/src/main.rs
+++ assembler/src/main.rs
@@ -1,7 +1,7 @@
 use assembler::Assembler;
 
 fn main() {
-    let source = "start:\nnop move.dw D0, D7\nmove.dw D2, $AABBCCDD\nmove.dw D3, #-5055324";
+    let source = "push.dw (a0)+\npush.dw(a0)-";
     let mut assembler = Assembler::new();
     assembler.assemble(source);
 }
