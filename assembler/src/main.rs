43c73981d8da7f7b30a495388f8b6b46a70a36ad
Kay Grewe <Kay.Grewe@protonmail.com>
2024-10-04 22:48:32 +0200

    feat(assembler): the assembler now takes an input file as an argument

--- assembler/src/main.rs
+++ assembler/src/main.rs
@@ -1,7 +1,31 @@
 use assembler::Assembler;
-
+use std::env;
+use std::fs;
+use std::io::prelude::*;
 fn main() {
-    let source = "push.dw (a0)+\npush.dw(a0)-";
+    let args: Vec<String> = env::args().skip(1).collect();
+
+    if args.is_empty() {
+        println!("USAGE: assembler [source_file].dasm");
+        return;
+    }
+
+    let source = fs::read_to_string(&args[0]).unwrap();
+
+    //let source = "push.dw (a0)+\npush.dw(a0)-";
     let mut assembler = Assembler::new();
-    assembler.assemble(source);
+    let machine_code = assembler.assemble(&source);
+
+    let mut output_file = fs::File::create("output.bin").unwrap();
+
+    for code in machine_code {
+        let bytes: [u8; 4] = [
+            (code >> 24 & 0xFF) as u8,
+            (code >> 16 & 0xFF) as u8,
+            (code >> 8 & 0xFF) as u8,
+            (code & 0xFF) as u8,
+        ];
+
+        output_file.write_all(&bytes).unwrap();
+    }
 }
